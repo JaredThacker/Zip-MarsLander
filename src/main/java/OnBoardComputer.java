9233b0d2634461441de32915d00f8ee4c0441e4c
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-31 12:17:23 -0400

    kris solution

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -2,22 +2,38 @@ public class OnBoardComputer implements BurnStream {
 
     @Override
     public int getNextBurn(DescentEvent status) {
-        int burn = 0;
-        if (status.getVelocity() == 2){
-            burn = 100; // check if at 2 ms
-        } else if (status.getAltitude() > 6000) {
-            burn = 100; // check how high to maintain to descend
-        } else if (status.getVelocity() > 100) {
-            burn = 200; // determines if u need to slow down
-        } else if (status.getAltitude() > 100) {
-            burn = 100; // re check altitude to see if at correct velocity
-        } else {
-            burn = 198; // slow down but not completely stop
-        }
+        //the kris solution
+        int sp = status.getVelocity();
+        int a = status.getAltitude();
+        int burn = 100;
+        int zz = 0; // burn delta from 100;
 
-        System.out.println(burn); /*hack!*/
+        // a = (speed * speed)/(2 * altitude) // from readme.
+        zz = (sp * sp) / ( 2 * a);
+        burn = zz + 99; // use 99 to always drift downward (due to integer div)
+        //System.out.print("["+zz+"] "); /*print estimated burn above 100 */
+        if (burn > 200) burn = 200; // if Newton wants more fuel than 200, deny it
         return burn;
     }
+
+//    @Override
+//    public int getNextBurn(DescentEvent status) {
+//        int burn = 0;
+//        if (status.getVelocity() == 2){
+//            burn = 100; // check if at 2 ms
+//        } else if (status.getAltitude() > 6000) {
+//            burn = 100; // check how high to maintain to descend
+//        } else if (status.getVelocity() > 100) {
+//            burn = 200; // determines if u need to slow down
+//        } else if (status.getAltitude() > 100) {
+//            burn = 100; // re check altitude to see if at correct velocity
+//        } else {
+//            burn = 198; // slow down but not completely stop
+//        }
+//
+//        System.out.println(burn); /*hack!*/
+//        return burn;
+//    }
 
 //    @Override
 //    public int getNextBurn(DescentEvent status){
